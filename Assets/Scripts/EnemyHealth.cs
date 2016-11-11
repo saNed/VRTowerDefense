9f55569708d39129c59522cad08eb14f7ddc0f7d
saNed <p@uloalv.es>
2016-11-11 13:04:38 -0600

    Add basic scene with player, tower, and moving enemy

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+	public int startingHealth = 100;
+	public int currentHealth;
+
+	void Awake ()
+	{
+		currentHealth = startingHealth;
+	}
+		
+	public void TakeDamage (int amount)
+	{
+
+		currentHealth -= amount;
+
+		if(currentHealth <= 0)
+		{
+			Destroy (gameObject, 2f);
+		}
+	}
+}
---
+++ Assets/Scripts/TowerShoot.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TowerShoot : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
