c32c3afd461bc751f63a62c211b5fbac07784276
saNed <p@uloalv.es>
2016-11-22 17:43:44 -0600

    Create network of nodes to plant turrets on. Clicking on node spawns turret.

---
+++ Assets/Scripts/Node/Node.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Node : MonoBehaviour {
+
+	public Color hoverColor;
+	public GameObject tower;
+
+	private Renderer rend;
+	private Color startColor;
+	private GameObject turret;
+
+	void Start () {
+		rend = GetComponent<Renderer> ();
+		startColor = rend.material.color;
+	}
+
+	void OnMouseEnter ()
+	{
+		rend.material.color = hoverColor;
+	}
+
+	void OnMouseDown ()
+	{
+		if (turret != null) {
+			Debug.Log ("Can't build there!");
+			return;
+		}
+		Instantiate (tower, transform.position, Quaternion.identity, this.transform.parent);
+	}
+
+	void OnMouseExit ()
+	{
+		rend.material.color = startColor;
+	}
+}
---
+++ Assets/Scripts/Node/NodeManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NodeManager : MonoBehaviour {
+
+	public GameObject node;
+	public GameObject nodeRenderer;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = -9; i <= 9; i++) {
+			for (int j = -9; j <= 9; j++) {
+				GameObject n = (GameObject) Instantiate(node, new Vector3 (i, 0, j), Quaternion.identity, this.transform);
+				n.name = i + " x " + j;
+
+				GameObject nr = (GameObject) Instantiate (nodeRenderer, new Vector3 (i, 0, j), Quaternion.identity, n.transform);
+			}
+		}
+	}
+}
