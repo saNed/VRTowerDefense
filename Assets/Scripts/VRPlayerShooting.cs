7749d56b23cb17679d5b555d94942f01b55ba5ab
saNed <p@uloalv.es>
2016-11-28 19:28:36 -0600

    VR works

---
+++ Assets/Scripts/VRPlayerShooting.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VRPlayerShooting : MonoBehaviour {
+
+    public GameObject projectile;
+    public GameObject projectileSpawn;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1")) {
+            Debug.Log("I FIRED!");
+            Instantiate(projectile, projectileSpawn.transform.position, Quaternion.Euler(projectileSpawn.transform.forward));
+        }
+	}
+
+}
