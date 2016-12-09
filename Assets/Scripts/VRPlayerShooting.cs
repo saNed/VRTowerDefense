b3d196c7c647cb6056455a54e76f19398007cd29
Nilo Lisboa <nilo_bl@hotmail.com>
2016-12-08 22:09:34 -0600

    networked VR player

--- Assets/Scripts/VRPlayerShooting.cs
+++ Assets/Scripts/VRPlayerShooting.cs
@@ -1,25 +1,1 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class VRPlayerShooting : MonoBehaviour {
-
-    public GameObject projectile;
-    public GameObject projectileSpawn;
-	public int damage;
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetAxis("Fire2") != 0.0f)
-        {
-            Debug.Log("I FIRED!");
-			GameObject proj = Instantiate(projectile, projectileSpawn.transform.position, Quaternion.Euler(projectileSpawn.transform.forward)) as GameObject;
-			proj.GetComponent<BaseProjectile>().fireProjectile2(projectileSpawn, damage);
-        }
-	}
-
-}
+﻿using UnityEngine;using System.Collections;public class VRPlayerShooting : MonoBehaviour {    public GameObject projectile;    public GameObject projectileSpawn;	public int damage;    public GameObject gun;	// Use this for initialization	void Start () {		}		// Update is called once per frame	void Update () {        if (Input.GetAxis("Fire2") != 0.0f)        {            Debug.Log("I FIRED!");			GameObject proj = Instantiate(projectile, projectileSpawn.transform.position, Quaternion.Euler(projectileSpawn.transform.forward)) as GameObject;			proj.GetComponent<BaseProjectile>().fireProjectile2(projectileSpawn, damage);            gun.transform.Rotate(Vector3.forward * Time.deltaTime * 500);        }	}}
